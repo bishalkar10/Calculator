075f97aeefa0ef67f1e20250a581b54f4801344a
Bishal Kar <bishalkar10@gmail.com>
2023-03-22 14:55:05 +0530

    HTML, CSS is completed but Javascript is reamining

---
+++ main.js
@@ -0,0 +1,7 @@
+const numberButton = document.querySelectorAll('[data-number]')
+const operationButton = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const currentOperandButton = document.querySelector('[data-current-operand]')
+const previousOperandButton = document.querySelector('[data-previous-operand]') 
